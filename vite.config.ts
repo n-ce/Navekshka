43a04e60696584d07919862f7b4fbe9a77b92e14
n-ce <abrocks1120@gmail.com>
2026-04-05 19:41:53 +0530

    chore: support github pages repo path with relative base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solid from 'vite-plugin-solid'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
+  base: './',
   plugins: [
     solid(),
     viteSingleFile()
